a1eee88c407afb2c1b3f89f954fcb3db5def15e2
Rumbo74 <rumbo74@gmx.de>
2021-03-12 17:56:22 +0100

    neue tests übernommen

--- test/all.js
+++ test/all.js
@@ -1,5 +1,33 @@
-const path = require("path");
-const { tests } = require("@iobroker/testing");
+const path = require('path');
 
-// Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const question = prompt => {
+  return new Promise((resolve, reject) => {
+    rl.question(prompt, resolve)
+  })
+}
+
+(async () => {
+  const nameAnswer = await question('What is your name?')
+  console.log(`Nice to meet you, ${nameAnswer}.`)
+
+  const whereAnswer = await question('Where are you from?')
+  console.log(`I hear it's nice in ${whereAnswer}.`)
+
+  rl.close()
+})()
+
+// using assert passed to the test function that just logs failures
+//exports['test that logs all failures'] = function(assert) {
+//  assert.equal(2 + 2, 5, 'assert failure is logged')
+//  assert.equal(3 + 2, 5, 'assert pass is logged')
+//}
+
+//if (module == require.main) require('test').run(exports)
+
